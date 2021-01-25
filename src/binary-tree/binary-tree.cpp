547f2f3f0a90182cf06f1a2323f3e1ccf856c60b
sosukesuzuki <aosukeke@gmail.com>
2021-01-25 20:36:12 +0900

    Implmenet inorder

--- src/binary-tree/binary-tree.cpp
+++ src/binary-tree/binary-tree.cpp
@@ -1,7 +1,7 @@
 #include "binary-tree.h"
 #include <string>
 
-Node::Node(const std::string newValue): value(newValue) {}
+Node::Node(const std::string newValue) : value(newValue) {}
 Node::Node(const std::string newValue, Node *newLeft, Node *newRight)
     : left(newLeft), right(newRight), value(newValue) {}
 Node::~Node() {
@@ -18,5 +18,13 @@ void preorder(Node *node, CallBackFn cb) {
     preorder(node->right, cb);
   }
 }
-void inorder(Node *node, CallBackFn cb) {}
+void inorder(Node *node, CallBackFn cb) {
+  if (node->left != NULL) {
+    inorder(node->left, cb);
+  }
+  cb(node->value);
+  if (node->right != NULL) {
+    inorder(node->right, cb);
+  }
+}
 void postorder(Node *node, CallBackFn cb) {}
--- src/binary-tree/main.cpp
+++ src/binary-tree/main.cpp
@@ -1,18 +1,27 @@
 #include "binary-tree.h"
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cassert>
 
 using namespace std::string_literals;
 
 int main() {
-  Node *tree1 = new Node("foo"s, new Node("bar"s), new Node("baz"s));
+  Node *tree = new Node("foo"s, new Node("bar"s), new Node("baz"s));
+
   std::vector<std::string> values = {};
-  preorder(tree1, [&values](std::string value) { values.push_back(value); });
+
+  preorder(tree, [&values](std::string value) { values.push_back(value); });
   assert(values[0] == "foo");
   assert(values[1] == "bar");
   assert(values[2] == "baz");
   values.clear();
-  delete tree1;
+
+  inorder(tree, [&values](std::string value) { values.push_back(value); });
+  assert(values[0] == "bar");
+  assert(values[1] == "foo");
+  assert(values[2] == "baz");
+  values.clear();
+
+  delete tree;
 }
