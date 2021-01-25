b333013a2e79665564e1f2a9e4091ac30e36225c
sosukesuzuki <aosukeke@gmail.com>
2021-01-25 23:27:05 +0900

    Add empty to queue

--- src/queue/main.cpp
+++ src/queue/main.cpp
@@ -4,12 +4,15 @@
 
 int main() {
   queue::Queue *queue = new queue::Queue(10);
+  assert(queue->empty());
   queue->enqueue("foo");
   queue->enqueue("bar");
   queue->enqueue("baz");
+  assert(!queue->empty());
   assert(queue->dequeue() == "foo");
   assert(queue->dequeue() == "bar");
   assert(queue->dequeue() == "baz");
+  assert(queue->empty());
   delete queue;
 
   queue::Queue *small_queue = new queue::Queue(0);
--- src/queue/queue.h
+++ src/queue/queue.h
@@ -13,6 +13,7 @@ private:
 public:
   void enqueue(std::string data);
   std::string dequeue();
+  bool empty();
   Queue(const int newSize);
   ~Queue();
 };
