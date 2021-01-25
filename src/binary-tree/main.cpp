3c3bdc21bbf64439d8d85784d88919df4e0c40cd
Sosuke Suzuki <aosukeke@gmail.com>
2021-01-25 22:51:50 +0900

    Binary tree (#4)
    * Implement binary-tree
    
    * Implmenet inorder
    
    * Implement postorder

---
+++ src/binary-tree/main.cpp
@@ -0,0 +1,33 @@
+#include "binary-tree.h"
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std::string_literals;
+
+int main() {
+  Node *tree = new Node("foo"s, new Node("bar"s), new Node("baz"s));
+
+  std::vector<std::string> values = {};
+
+  preorder(tree, [&values](std::string value) { values.push_back(value); });
+  assert(values[0] == "foo");
+  assert(values[1] == "bar");
+  assert(values[2] == "baz");
+  values.clear();
+
+  inorder(tree, [&values](std::string value) { values.push_back(value); });
+  assert(values[0] == "bar");
+  assert(values[1] == "foo");
+  assert(values[2] == "baz");
+  values.clear();
+
+  postorder(tree, [&values](std::string value) { values.push_back(value); });
+  assert(values[0] == "bar");
+  assert(values[1] == "baz");
+  assert(values[2] == "foo");
+  values.clear();
+
+  delete tree;
+}
