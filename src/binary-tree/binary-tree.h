2ccba4f60ed27acc162d0898158228bef8dbf14f
Sosuke Suzuki <aosukeke@gmail.com>
2021-02-19 21:10:04 +0900

    Implement breadth_first_search (#7)
    * Implement breadh_first_search
    
    * Fix typo

--- src/binary-tree/binary-tree.h
+++ src/binary-tree/binary-tree.h
@@ -18,6 +18,7 @@ using CallBackFn = std::function<void(std::string)>;
 void preorder(Node *node, CallBackFn cb);
 void inorder(Node *node, CallBackFn cb);
 void postorder(Node *node, CallBackFn cb);
+void breadth_first_search(Node *root_node, CallBackFn cb);
 } // namespace binary_tree
 
 #endif
